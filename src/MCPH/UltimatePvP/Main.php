d08a1c2641cedac1bc6e5439f032497770abbd62
Anton Magsino <MinecrafterPH@users.noreply.github.com>
2015-12-05 19:02:58 +0800

    Update Main.php

--- src/MCPH/UltimatePvP/Main.php
+++ src/MCPH/UltimatePvP/Main.php
@@ -6,7 +6,9 @@ use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\network\protocol\AddEntityPacket;
@@ -18,12 +20,16 @@ class Main extends PluginBase implements Listener
 {
   
   public $config;
+  public $tasks = array();
+  public $interval = 10;
+  private $players = array();
   
   public function onEnable()
   {
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
     $this->getLogger()->info("UltimatePvP has been enabled.");
-    $this->config = new Config($this->getDataFolder()."config.yml", Config::YAML);
+    $this->config = new Config($this->getDataFolder()."kills.yml", Config::YAML);
+    $this->interval = $this->getConfig()->get("interval");
   }
   
   public function onDisable()
@@ -31,8 +37,47 @@ class Main extends PluginBase implements Listener
     $this->getLogger()->info("UltimatePvP has been disabled.");
   }
   
+  /**
+   * @param EnityDamageEvent $event
+   *
+   * @priority LOW
+   * @ignoreCancelled true
+   */
+  public function onDamage(EntityDamageEvent $event)
+  {
+    if($event instanceof EntityDamageByEntityEvent){
+      if($event->getDamager() instanceof Player){
+        foreach(array($event->getDamager(),$event->getEntity()) as $players){
+          $this->setTime($players);
+        }
+      }
+    }
+  }
+  
+  private function setTime(Player $player)
+  {
+    if(isset($this->players[$player->getName()])){
+      if((time() - $this->players[$player->getName()]) > $this->interval){
+        $player->sendMessage(TextFormat::RED . "You are in PvP! Do not log out!");
+      }
+      if(isset($this->tasks[$player->getName()])){
+        $this->getServer()->getScheduler()->cancelTask($this->tasks[$player->getName()]);
+      }
+      $this->tasks[$player->getName()] = $this->getServer()->getScheduler()->scheduleRepeatingTask(new TimeMsg($this, $player), 20)->getTaskId();
+      }else{
+        $player->sendMessage(TextFormat::RED . "You are in PvP! Do not log out!");
+        $this->tasks[$player->getName()] = $this->getServer()->getScheduler()->scheduleRepeatingTask(new TimeMsg($this, $player), 20)->getTaskId();
+      }
+    $this->players[$player->getName()] = time();
+  }
+  
   public function onDeath(PlayerDeathEvent $e)
   {
+    if(isset($this->players[$event->getEntity()->getName()])){
+      unset($this->players[$event->getEntity()->getName()]);
+      if(isset($this->tasks[$event->getEntity()->getName()])) $this->getServer()->getScheduler()->cancelTask($this->tasks[$event->getEntity()->getName()]);unset($this->tasks[$event->getEntity()->getName()]);
+    }
+    
     $player = $e->getEntity();
     if($player instanceof Player) {
       $cause = $p->getLastDamageCause();
@@ -62,6 +107,25 @@ class Main extends PluginBase implements Listener
       }
     }  
   }
+  
+  /**
+   * @param PlayerQuitEvent $event
+   *  
+   * @priority HIGH
+   * @ignoreCancelled true
+   */
+  public function PlayerQuitEvent(PlayerQuitEvent $event)
+  {
+    if(isset($this->players[$event->getPlayer()->getName()])){
+      $player = $event->getPlayer();
+      if((time() - $this->players[$player->getName()]) < $this->interval){
+        $player->kill();
+      }
+    unset($this->players[$player->getName()]);
+    if(isset($this->tasks[$player->getName()])) $this->getServer()->getScheduler()->cancelTask($this->tasks[$player->getName()]);unset($this->tasks[$player->getName()]);
+    }
+  }
+
   
   public function onCommand(CommandSender $sender, Command $command, $label, array $args)
   {
